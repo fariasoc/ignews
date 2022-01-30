ce489998507c869d8f0123871f23743f5af15cfa
Celso Farias <fariasoc@outlook.com>
2022-01-30 19:26:40 -0300

    botão de inscrição

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -11,11 +12,12 @@ export default function Home() {
       <main className={styles.contentContainer} >
         <section className={styles.hero} >
           <span> 👏 Olá, seja bem vindo  </span>
-          <h1> Novidades sobre o mundo <span> React </span>   </h1>
+          <h1> Novidades sobre o universo <span> React </span>   </h1>
           <p> 
-            Acesse todas as publicações <br /> 
-            <span> por R$ 9,90 por mês </span>
+            Acesse todas as publicações por <br /> 
+            <span>  R$ 9,90 por mês </span>
           </p>
+          <SubscribeButton/>
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
