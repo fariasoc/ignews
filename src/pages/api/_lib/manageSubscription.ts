40021af775ee895d1be7fd5252cedede8bdc5bf4
Celso Farias <fariasoc@outlook.com>
2022-02-02 22:17:42 -0300

    Index da inscrição do usuário

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -15,7 +15,7 @@ export async function saveSubscription(
       "ref",
       q.Get(
         q.Match(
-          q.Index('user_by_stripe_costumer_id'),
+          q.Index('user_by_stripe_customer_id'),
           customerId
         )
       )
@@ -23,11 +23,18 @@ export async function saveSubscription(
   )
 
   const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+  
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  }
 
   await fauna.query(
     q.Create(
       q.Collection('subscriptions'),
-      {data: subscription}
+      {data: subscriptionData}
       )
   )
 
